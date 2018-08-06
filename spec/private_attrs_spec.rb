f997805988344ffdaf9a3ba854070c30c38cd935
Zach Colon <zach.colon@adwerx.com>
2018-08-06 12:05:07 -0400

    Adds private protected attr methods
    - protected_attr_reader
    - protected_attr_writer
    - protected_attr_accessor

--- spec/private_attrs_spec.rb
+++ spec/private_attrs_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe PrivateAttrs do
       instance = TestClass.new
       instance.foo = 'foo'
 
-      expect{ instance.foo }.to raise_error(NoMethodError)
+      expect{ instance.foo }.to raise_error(NoMethodError, /private method `foo' called for/)
       expect{ instance.send(:foo) }.to_not raise_error
       expect(instance.send(:foo)).to eq 'foo'
     end
@@ -30,7 +30,7 @@ RSpec.describe PrivateAttrs do
 
       instance = TestClass.new
 
-      expect{ instance.foo = 'foo' }.to raise_error(NoMethodError)
+      expect{ instance.foo = 'foo' }.to raise_error(NoMethodError, /private method `foo=' called for/)
       expect{ instance.send(:foo=, 'foo') }.to_not raise_error
 
       instance.send(:foo=, 'foo')
@@ -51,7 +51,7 @@ RSpec.describe PrivateAttrs do
       instance = TestClass.new
       instance.foo = 'foo'
 
-      expect{ instance.foo }.to raise_error(NoMethodError)
+      expect{ instance.foo }.to raise_error(NoMethodError, /private method `foo' called for/)
       expect{ instance.send(:foo) }.to_not raise_error
       expect(instance.send(:foo)).to eq 'foo'
     end
@@ -66,7 +66,87 @@ RSpec.describe PrivateAttrs do
       end
 
       instance = TestClass.new
-      expect{ instance.foo = 'foo' }.to raise_error(NoMethodError)
+      expect{ instance.foo = 'foo' }.to raise_error(NoMethodError, /private method `foo=' called for/)
+
+      expect{ instance.send(:foo=, 'foo') }.to_not raise_error
+      instance.send(:foo=, 'foo')
+
+      expect(instance.foo).to eq 'foo'
+    end
+  end
+
+  describe 'protected_attr_reader' do
+    specify 'creates a protected reader method on an object' do
+      class TestClass
+        protected_attr_reader :foo
+
+        def foo=(value)
+          @foo = value
+        end
+      end
+
+      class SubClass < TestClass; end
+      instance = SubClass.new
+      instance.foo = 'foo'
+
+      expect{ instance.foo }.to raise_error(NoMethodError, /protected method `foo' called for/)
+      expect{ instance.send(:foo) }.to_not raise_error
+      expect(instance.send(:foo)).to eq 'foo'
+    end
+  end
+
+  describe 'protected_attr_writer' do
+    specify 'creates a protected writer method on an object' do
+      class TestClass
+        protected_attr_writer :foo
+
+        def foo
+          @foo
+        end
+      end
+
+      class SubClass < TestClass; end
+      instance = SubClass.new
+
+      expect{ instance.foo = 'foo' }.to raise_error(NoMethodError, /protected method `foo=' called for/)
+      expect{ instance.send(:foo=, 'foo') }.to_not raise_error
+
+      instance.send(:foo=, 'foo')
+      expect(instance.foo).to eq 'foo'
+    end
+  end
+
+  describe 'protected_attr_accessor' do
+    specify 'creates a protected reader method on an object' do
+      class TestClass
+        protected_attr_accessor :foo
+
+        def foo=(value)
+          @foo = value
+        end
+      end
+
+      class SubClass < TestClass; end
+      instance = SubClass.new
+      instance.foo = 'foo'
+
+      expect{ instance.foo }.to raise_error(NoMethodError, /protected method `foo' called for/)
+      expect{ instance.send(:foo) }.to_not raise_error
+      expect(instance.send(:foo)).to eq 'foo'
+    end
+
+    specify 'creates a protected writer method on an object' do
+      class TestClass
+        protected_attr_accessor :foo
+
+        def foo
+          @foo
+        end
+      end
+
+      class SubClass < TestClass; end
+      instance = SubClass.new
+      expect{ instance.foo = 'foo' }.to raise_error(NoMethodError, /protected method `foo=' called for/)
 
       expect{ instance.send(:foo=, 'foo') }.to_not raise_error
       instance.send(:foo=, 'foo')
